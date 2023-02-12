3788aa08ab6869b0f93b62ad4ec400f58cccf1b3
Margarita May Lee <ritochka.m_lee@yahoo.com>
2023-02-12 15:55:30 +0300

    First Commit

--- src/__test__/test.js
+++ src/__test__/test.js
@@ -1,13 +1,40 @@
-import orderByProps from "../index.js";
+import attackOPtions from '../index.js'
 
-test('should check that object propeties within values is sorted by requested propeties or in alphabet order', () => {
-    const obj = {name: 'мечник', health: 10, level: 2, attack: 80, defence: 40};
+test('to check that data from object property "special" is extracted into array', () => {
+    const character = {
+        name: 'Лучник',
+        type: 'Bowman',
+        health: 50,
+        level: 3,
+        attack: 40,
+        defence: 10,
+        special: [
+          {
+            id: 8,
+            name: 'Двойной выстрел',
+            icon: 'http://...',
+            description: 'Двойной выстрел наносит двойной урон'
+          }, 
+          {
+            id: 9,
+            name: 'Нокаутирующий удар',
+            icon: 'http://...'
+          }
+        ]	
+    }
     const expectedArr = [
-        {key: "name", value: "мечник"},
-        {key: "level", value: 2},
-        {key: "attack", value: 80},
-        {key: "defence", value: 40},
-        {key: "health", value: 10}
+        {
+            id: 8,
+            name: 'Двойной выстрел',
+            icon: 'http://...',
+            description: 'Двойной выстрел наносит двойной урон'
+        }, 
+        {
+            id: 9,
+            name: 'Нокаутирующий удар',
+            icon: 'http://...',
+            description: 'Описание недоступно'
+        }
     ];
-    expect(orderByProps(obj, ["name", "level"])).toEqual(expectedArr);
+    expect(attackOPtions(character)).toEqual(expectedArr);
 });
